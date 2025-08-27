145d6a0a66346226015ccde7b562649851ae2883
FrancescaDr <francesca.drumer@googlemail.com>
2025-08-27 13:47:55 +0200

    numpy dependency <2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
         "torch-geometric>=2.0.0",
         "pytorch-lightning>=1.5.0",
         "torchmetrics>=0.7.0",
-        "numpy>=1.19.0",
+        "numpy>=1.19.0,<2.0.0", # recommended version 1.26.4
         "scipy>=1.7.0",
     ],
     extras_require={
