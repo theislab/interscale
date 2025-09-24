9393128ac2138ee07747d2afe47e28d8184698bf
FrancescaDr <francesca.drummer@googlemail.com>
2025-09-24 10:21:31 +0200

    remove old gt_long_range_model folder

--- src/graph_transformer_long_range_niches/__init__.py
+++
@@ -1,7 +0,0 @@
-from importlib.metadata import version
-
-from . import pl, pp, tl, model, modules
-
-__all__ = ["pl", "pp", "tl", "model", "modules"]
-
-__version__ = version("graph-transformer-long-range-niches")
