0da5a1242ad0ab8de8898f6ad68bd044a575ed17
FrancescaDr <francesca.drummer@googlemail.com>
2025-11-03 16:48:31 +0100

    print batch size

--- src/InterScale/module/base/_base_local_module.py
+++ src/InterScale/module/base/_base_local_module.py
@@ -58,9 +58,12 @@ class LocalModuleClass(BaseModuleClass):
         
         local_embedding = self.forward(batch_masked.x, batch_masked.edge_index)
         y_pred = self.decoder.forward(local_embedding)
+        print('y_pred', y_pred.shape, y_pred)
+        print('local_embedding', local_embedding.shape, local_embedding)
         
         assert y_pred.shape[0] == len(batch.obs_names), f"Mismatch: y_pred.shape: {y_pred.shape[0]}, batch.obs_names: {len(batch.obs_names)}"
         assert y_pred.shape[1] == self.n_output, f"Mismatch: y_pred.shape: {y_pred.shape[1]}, self.n_output: {self.n_output}"
+        assert y_pred.isnan().sum() == 0, "y_pred contains NaN values"
         
         y_pred = y_pred[mask_idx]
         
