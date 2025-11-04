GlobalModule fix attention mask

src/InterScale/module/global_modules/transformer_encoder.py
@@ -106,10 +106,17 @@ class TransformerNodeEncoderHook(GlobalModuleClass):
                 mask = None, 
                 register_hook: bool = False):
         """
+        N_b_max: maximum number of nodes in the batch
+        B: batch size
+        H_d: dimension of transformer
+        
         Input: 
-            padded_h_node: [n_b x B X h_d] with n_b: dimension of batch, B: batch size, h_d: dimension of transformer
-            src_padding_mask: [B x n_b] matrix indicating the size of the padding mask to be ignored during calculation 
-            mask: [n_b x n_b] matrix indicating the long-range connections (inverse of adjacency matrix). Default: None
+            padded_h_node: [N_b_max x B X H_d] 
+                GEX embeddings of the nodes in the batch with padding (0) to the maximum number of nodes in the batch.
+            src_padding_mask: [B x N_b_max] 
+                Matrix indicating the size of the padding mask to be ignored during calculation.
+            mask: H_d, N_b_max x N_b_max] 
+                matrix indicating the long-range connections (inverse of adjacency matrix). Default: None
         """
         if register_hook:
             for encoder in self.transformer_encoder.layers:
@@ -120,6 +127,7 @@ class TransformerNodeEncoderHook(GlobalModuleClass):
         padded_h_node = torch.cat([padded_h_node, expand_cls_embedding], dim=0) #append cls embedding at the end of the sequence
         # normalize input
         padded_h_node = self.norm_input(padded_h_node)
+        assert not torch.any(torch.isnan(padded_h_node)), "normalized padded_h_node contains NaN values"
 
         zeros = src_padding_mask.data.new(src_padding_mask.size(0), 1).fill_(0)
         src_padding_mask = torch.cat([src_padding_mask, zeros], dim=1)

src/InterScale/tl/utils.py
@@ -34,9 +34,13 @@ def check_and_update_cfg(cfg,
     cfg.freeze()
     return cfg
 
-def create_transformer_attention_mask_from_edges(edge_index: torch.Tensor, num_nodes: int, batch: torch.Tensor, index_nodes: list, num_heads: int) -> torch.Tensor:
+def create_transformer_attention_mask_from_edges(edge_index: torch.Tensor, 
+                                                 num_nodes: int, 
+                                                 batch: torch.Tensor, 
+                                                 index_nodes: list, 
+                                                 num_heads: int) -> torch.Tensor:
     """
-    Creates an attention mask that is inverse to the edge indices. Unmasked = 0 and masked = 1
+    Creates an attention mask that is inverse to the edge indices. Unmasked = 0 and masked = -inf
     If two nodes are connected in the adjacency matrix (edge_index = 1) then we have no attention (0) and vice versa. 
     
     Args:
@@ -48,11 +52,13 @@ def create_transformer_attention_mask_from_edges(edge_index: torch.Tensor, num_n
     Returns:
         torch.Tensor: Attention mask of shape [num_batch*num_heads, max_seq_len, max_seq_len] with 1s for no attention (True -> mask attention) and 0s for attention (False -> no mask)
     """
+    INVALID_MASK_VALUE = -float('inf')
+    
     num_batch = int(batch[-1].item() + 1)
     max_seq_len = max(len(nodes) for nodes in index_nodes)
     
-    # Initialize with 1s (no attention allowed) 
-    attention_mask = torch.ones((num_batch*num_heads, max_seq_len+1, max_seq_len+1), device=edge_index.device)
+    # Initialize with -inf (no attention allowed) 
+    attention_mask = torch.full((num_batch*num_heads, max_seq_len+1, max_seq_len+1), INVALID_MASK_VALUE, device=edge_index.device)
     
     # Create full adjacency matrix + 1 for cls token (end of sequence)
     adj_matrix = torch.zeros((num_nodes, num_nodes), device=edge_index.device) # TODO: check if zero or ones
@@ -62,12 +68,16 @@ def create_transformer_attention_mask_from_edges(edge_index: torch.Tensor, num_n
     for b in range(num_batch):
         nodes = index_nodes[b]
         seq_len = len(nodes)
+        assert seq_len+1 <= max_seq_len+1, f"Mismatch: seq_len+1: {seq_len+1}, max_seq_len+1: {max_seq_len+1}"
         # Extract submatrix for the kept nodes
         batch_mask = adj_matrix[nodes][:, nodes]  # Get submatrix for kept nodes
         # Add row and column of ones for CLS token - full attention
         batch_mask = torch.cat([batch_mask, torch.zeros(batch_mask.size(0), 1, device=batch_mask.device)], dim=1)  # Add column
         batch_mask = torch.cat([batch_mask, torch.zeros(1, batch_mask.size(1), device=batch_mask.device)], dim=0)  # Add row
-        attention_mask[b*num_heads:b*num_heads+num_heads, :seq_len+1, :seq_len+1] = batch_mask
+        assert batch_mask.shape == (seq_len+1, seq_len+1), f"Mismatch: batch_mask.shape: {batch_mask.shape}, (seq_len+1, seq_len+1): {(seq_len+1, seq_len+1)}"
+        assert attention_mask.shape[-2:] == (max_seq_len+1, max_seq_len+1), f"Mismatch: attention_maks.shape[-2:]: {attention_mask.shape[-2:]}, (seq_len+1, seq_len+1): {(seq_len+1, seq_len+1)}"
+        #attention_mask[b*num_heads:b*num_heads+num_heads, -seq_len+1:, -seq_len+1:] = batch_mask
+        attention_mask[b*num_heads:b*num_heads+num_heads, -(seq_len+1):, -(seq_len+1):] = batch_mask
     return attention_mask
 
 def get_model_filename_prefix(cfg, local_component: bool, global_component: bool):

tests/module/test_global_module.py
@@ -10,8 +10,7 @@ from InterScale.tl import pad_batch, apply_mask
 from InterScale.tl.masking import MASK_VALUE
 
 
-@pytest.fixture
-def sample_global_module_kwargs():
+def create_sample_global_module_kwargs(long_range_attention):
     """Create sample keyword arguments for global module testing."""
     return {
         'max_seq_len': 20,
@@ -19,12 +18,11 @@ def sample_global_module_kwargs():
         'dropout_global': 0.1,
         'num_layers': 2,
         'dim_feedforward': 64,
-        'long_range_attention': False
+        'long_range_attention': long_range_attention
     }
 
 
-@pytest.fixture
-def sample_graph(num_nodes=10, num_features=5, num_classes=3, graph_level=True):
+def create_sample_graph(num_nodes=10, num_features=5, num_classes=3, graph_level=True):
     """Create a sample PyG graph for testing."""
     x = torch.randn(num_nodes, num_features)
     
@@ -53,10 +51,12 @@ def sample_graph(num_nodes=10, num_features=5, num_classes=3, graph_level=True):
 
 
 @pytest.fixture
-def sample_batch(sample_graph, num_graphs=2):
+def sample_batch(num_graphs=2, num_features=5, num_classes=3, graph_level=True):
     """Create a batch of graphs for testing."""
     graphs = []
     for _ in range(num_graphs):
+        num_nodes = np.random.randint(5, 11)
+        sample_graph = create_sample_graph(num_nodes=num_nodes, num_features=num_features, num_classes=num_classes, graph_level=graph_level)
         graphs.append(sample_graph)
     batch = Batch.from_data_list(graphs)
     return batch
@@ -88,14 +88,17 @@ def test_common_step_masking(sample_global_module_kwargs, sample_batch, pct_mask
         assert mask_idx.shape == (sample_batch.x.shape[0],)
         
 @pytest.mark.parametrize('prediction_level', ['node', 'graph'])
-@pytest.mark.parametrize('pct_mask_nodes', [0.0, 0.2])
-def test_common_step_local_to_global(sample_global_module_kwargs, sample_batch, pct_mask_nodes, prediction_level):
+@pytest.mark.parametrize('pct_mask_nodes', [0.0, 0.6])
+@pytest.mark.parametrize('long_range_attention', [True, False])
+def test_common_step_local_to_global(sample_batch, pct_mask_nodes, prediction_level, long_range_attention):
     """Test common_step_local_to_global."""
     n_input = 5
     n_output = 3
     n_embed = 4
     
     batch_size = len(np.unique(sample_batch.batch))
+    
+    global_module_kwargs = create_sample_global_module_kwargs(long_range_attention=long_range_attention)
 
     module = TransformerNodeEncoderHook(
         n_input=n_input,
@@ -103,7 +106,7 @@ def test_common_step_local_to_global(sample_global_module_kwargs, sample_batch, 
         n_embed=n_embed,
         decoder_type='linear',
         pct_mask_nodes=pct_mask_nodes,
-        **sample_global_module_kwargs
+        **global_module_kwargs
     )
     
     batch_masked, mask_idx = module._common_step_masking(sample_batch)
@@ -122,10 +125,7 @@ def test_common_step_local_to_global(sample_global_module_kwargs, sample_batch, 
     
     global_embedding, src_padding_mask = module.forward(padded_emb, src_padding_mask, attention_mask)
     y_pred = module.predict(global_embedding, src_padding_mask, prediction_level)
-
+    print('src_padding_mask', src_padding_mask.shape, src_padding_mask)
     print('y_pred', y_pred.shape, y_pred)
     print('embedding', embedding.shape, embedding)
     print('global_embedding', global_embedding.shape, global_embedding)
-    #y_true, adjusted_mask_idx = module._process_batch_for_metrics(sample_batch, 'classification', prediction_level, pad_index_nodes, mask_idx)
-    # print('y_true', y_true.shape, y_true)
-    # print('adjusted_mask_idx', adjusted_mask_idx.shape, adjusted_mask_idx)