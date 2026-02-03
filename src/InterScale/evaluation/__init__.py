380a98c9af734d60d453bebe267ed54aaea7083c
FrancescaDr <francesca.drummer@googlemail.com>
2026-02-03 15:37:55 +0000

    remove WandB_evaluation from evaluation

--- src/InterScale/evaluation/__init__.py
+++ src/InterScale/evaluation/__init__.py
@@ -5,7 +5,6 @@ from ._gene_set_covariance import gene_set_covariance, spatial_covariance_plot
 from .clustering import leiden_cluster_embeddings, plot_clustering_metrics
 from .gene_rank_analysis import predict_gene_r2
 from .graph_classification import calculate_pr_auc, pr_auc_curve, scale_cls_by_sample
-from .wandb_sweep_evaluation import Wandb_evaluation
 
 __all__ = [
     "leiden_cluster_embeddings",
@@ -13,5 +12,4 @@ __all__ = [
     "predict_gene_r2",
     "calculate_pr_auc",
     "scale_cls_by_sample",
-    "Wandb_evaluation",
 ]
