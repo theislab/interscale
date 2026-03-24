971dc61d8d87514b097b767c99325319d5873dfa
Francesca Drummer <francesca.drummer@googlemail.com>
2026-03-24 17:28:29 +0100

    mcml sweep

--- run_mcml_sweep.sh
+++ run_mcml_sweep.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --partition=mcml-dgx-a100-40x8
+#SBATCH --partition=mcml-hgx-a100-80x4
 #SBATCH --qos=mcml
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
@@ -23,12 +23,13 @@ export WANDB_API_KEY="45b9c9a439c12187aa03a740a0cacad57dcf958f"
 DEFAULT_CONFIG_ICB="/home/icb/francesca.drummer/1-Projects/GT-long-range-niches/src/config_files/"
 DEFAULT_CONFIG_LRZ="/dss/dsshome1/05/di93tig/1_projects/GT-long-range-niches/src/config_files/"
 
-SWEEP="Chen22/sweep/robustness.yaml"
+SWEEP="melton25/sweep/robustness.yaml"
 
 LEGNINI_CONFIG="Legnini_23/legnini23_graph_sample_LocalModel_gnn.yaml"
 SCHUERCH_CONFIG="Schuerch20/schuerch20_graph_sample_GlobalModel.yaml"
 SCHUERCH_SWEEP="Schuerch20/schurch20_hyperparam_sweep.yaml"
 CHEN_CONFIG="Chen22/best_graph_InterScale.yaml"
+MELTON_CONFIG="melton25/clas_graph.yaml
 
 
 echo "Current working directory: $(pwd)"
@@ -39,9 +40,9 @@ srun -N1 --ntasks-per-node=1 \
 	     --container-mounts=/dss/dsshome1/05/di93tig/1_projects/GT-long-range-niches:/workspace,/dss/dssfs03:/dss/dssfs03 \
 	          --container-image='/dss/dssfs03/tumdss/pn36po/pn36po-dss-0002/di93tig/custom-enroot-image/InterScale.sqsh' \
 		       python src/InterScale/main_sweep.py \
-		            --cfg "${DEFAULT_CONFIG_LRZ}${CHEN_CONFIG}" \
+		            --cfg "${DEFAULT_CONFIG_LRZ}${MELTON_CONFIG}" \
 			         --sweep_cfg "${DEFAULT_CONFIG_LRZ}${SWEEP}" \
-				      --model_type "CombinedModel" \
+				      --model_type "GlobalModel" \
 				           --sweep_goal robustness
 					        --prediction_task classification
 
