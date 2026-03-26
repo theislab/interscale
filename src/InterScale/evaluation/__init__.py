807329f2acf6319ebec155312110a15ef8720bc7
Sara Jimènez <sara.jimenez@hpc-submit01.scidom.de>
2026-03-26 15:58:20 +0100

    gene_eval_gene_loadings

--- src/InterScale/evaluation/__init__.py
+++ src/InterScale/evaluation/__init__.py
@@ -2,6 +2,7 @@
 
 from ._gene_loadings import gene_loadings
 from ._gene_set_covariance import gene_set_covariance, spatial_covariance_plot
+from ._latent_analysis import latent_rank_report, pl_latent_correlation, pl_dim_importance_elbow_stdexpr, get_genes_dim
 from .clustering import leiden_cluster_embeddings, plot_clustering_metrics
 from .gene_rank_analysis import predict_gene_r2
 from .graph_classification import calculate_pr_auc, pr_auc_curve, scale_cls_by_sample
@@ -15,5 +16,9 @@ __all__ = [
     "plot_clustering_metrics",
     "predict_gene_r2",
     "calculate_pr_auc",
-    "scale_cls_by_sample"
+    "scale_cls_by_sample",
+    "latent_rank_report",
+    "pl_latent_correlation",
+    "pl_dim_importance_elbow_stdexpr",
+    "get_genes_dim",
 ]
