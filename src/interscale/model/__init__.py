5fefef2a99292f0149b4a80b96455535909b3740
FrancescaDr <francesca.drummer@googlemail.com>
2026-04-27 12:31:55 +0100

    rename model files

--- src/interscale/model/__init__.py
+++ src/interscale/model/__init__.py
@@ -1,6 +1,6 @@
 from .base._base_model import BaseModel
-from .CombinedModel import CombinedModel
-from .GlobalModel import GlobalModel
-from .LocalModel import LocalModel
+from .combined_model import CombinedModel
+from .global_model import GlobalModel
+from .local_model import LocalModel
 
 __all__ = ["BaseModel", "LocalModel", "GlobalModel", "CombinedModel"]
